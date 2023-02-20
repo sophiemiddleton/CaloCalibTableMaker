fcccca5b31c252775d0c6369be4808a080807c2e
sophieMu2e <sophie@fnal.gov>
2023-02-20 14:03:23 -0600

    first attempt

--- CaloCalibTableMaker.cc
+++ CaloCalibTableMaker.cc
@@ -1,43 +1,77 @@
 //To run:  g++ -std=c++14 CaloCalibTableMaker.cc -o a.out the ./a.out (add -pthread to use multithreaded)
-#include "Offline/CaloReco/inc/CaloCalibTableMaker.hh" 
-#include <iostream>
-#include <tuple>
-using namespace std;
+#include "CaloCalibTableMaker.hh" 
 
-// reads an input archive table
-std::tuple<std::vector<double>, std::vector<double> > inputTable(const char *ArchiveFile){
-  std::tuple<std::vector<double> peaks, std::vector<double> widths> inputs;
+
+// inputTable : reads an input archive table
+Table inputTable(const char *ArchiveFile, const char *tag){
+  std::vector<int> roids;
   std::vector<double> peaks;
   std::vector<double> widths;
+  std::vector<double> errpeaks;
+  std::vector<double> errwidths;
+  std::vector<double> chisqs;
+  
   FILE *fC = fopen(ArchiveFile, "r");
 	if ( fC == NULL) {
 		std::cout<<"[In inputTable()] : Error: Cannot open file "<<std::endl;
 		exit(1);
 	}
-	float peak, width;
-	n=0;
-	while(fscanf(fC, "%f,%f\n", &peak, &width)!=EOF){
+	int roid;
+	float peak, errpeak, width, errwidth, chisq;
+	while(fscanf(fC, "%i,%f,%f,%f,%f,%f\n", &roid, &peak, &errpeak, &width, &errwidth, &chisq)!=EOF){
+		roids.push_back(roid);
 		peaks.push_back(peak);
+		errpeaks.push_back(errpeak);
 		widths.push_back(width);
+		errwidths.push_back(errwidth);
+		peaks.push_back(peak);
+		chisqs.push_back(chisq);
 	}
 	fclose(fC);
-  inputs = std::make_tuple(peaks, widths);
-  return inputs;
-//Table should be a class
+	Table input(roids, peaks, errpeaks, widths, errwidths, chisqs, tag);
+  return input;
+}
+// outputTable : returns final table in .txt form
+void outputTable(Table table){
+
 }
 
-void addTable(){
-//adds a Table of a given type to a list of tables to include in the calculation
-}
-void outputTable(){
-//returns final table in .txt form
-}
-
-void combineAlg(){
-// called by main function, this funciton does the hard work
+// combineAlg : called by main function, this funciton does the hard work
+Table combineAlg(std::vector<Table> tables){
+  Table output;
+  std::vector<int> roids;
+  std::vector<double> peaks;
+  std::vector<double> widths;
+  std::vector<double> errpeaks;
+  std::vector<double> errwidths;
+  std::vector<double> chisqs;
+  double p_av = 0;
+  double errp_av = 0;
+  double w_av = 0;
+  double errw_av = 0;
+  double chisq_av = 0; //TODO - how to deal with this?
+  
+  for(unsigned int i = 0; i < tables.size() ; i++){
+    std::cout<<"looking at table "<<tables[i].algtag_<<std::endl;
+    roids.push_back(tables[i].roid_);
+    
+    
+  }
+  Table output(roids, peaks, errpeaks, widths, errwidths, chisqs, "reco combined");
 }
 
 int main(){
-  std::cout<<"testing"<<std::endl;
+  std::vector<Table> tables;
+  // get source table
+  Table sourceInput = inputTable("testdata.csv", "source");
+  tables.push_back(sourceInput);
+  // get MIP table
+  Table mipInput = inputTable("testdata.csv", "mip");
+  tables.push_back(mipInput);
+  // get laser table
+  Table laserInput = inputTable("testdata.csv", "laser");
+  tables.push_back(laserInput);
+  // pass to combination:
+  combineAlg(tables);
   return 0;
 }
--- CaloCalibTableMaker.hh
+++ CaloCalibTableMaker.hh
@@ -1,13 +1,37 @@
 #ifndef CaloCalibTableMaker_hh
 #define CaloCalibTableMaker_hh
+#include<vector>
+#include <iostream>
+#include <utility>
+#include <tuple>
+using namespace std;
+struct Table{
+  std::vector<int> roid_;
+  std::vector<double> peak_;
+  std::vector<double> errpeak_;
+  std::vector<double> width_;
+  std::vector<double> errwidth_;
+  std::vector<double> chisq_;
+  const char* algtag_;
+  Table(){};
+  Table(
+  std::vector<int> roid, 
+  std::vector<double> peak, 
+  std::vector<double> errpeak, 
+  std::vector<double> width, 
+  std::vector<double> errwidth, 
+  std::vector<double> chisq, 
+  const char* tag) 
+  : roid_(roid), peak_(peak), errpeak_(errpeak), width_(width), errwidth_(errwidth), chisq_(chisq), algtag_(tag) {};
+  
+};
 
 class CaloCalibTableMaker
 	{
       public:
-        std::tuple<std::vector<double>, std::vector<double> > inputTable(const char *ArchiveFile);
-        void addTable();
-        void outputTable();
-        void combineAlg();
+        Table inputTable(const char *ArchiveFile, const char *tag);
+        void outputTable(Table *table);
+        void combineAlg(std::vector<Table> tables);
       private:
       //TODO
   };
